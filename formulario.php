3e583baa0c3282ac99d3cf607583ba9457077de5
cetecom <cetecom@LC08L5712>
2017-05-02 11:14:48 -0300

    update

---
+++ formulario.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form method="post" action="guardar.php">
+            <div>nombre: <input name="nombre" type="text"></div>
+            <div>apellido: <input name="apellido" type="text"></div>
+            <div>fec nacimiento: <input name="fecnacimiento" type="text"></div>
+            <input type="submit" value="Enviar">  
+        </form>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -10,18 +10,5 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-         $miconn=new mysqli("localhost", "root", "avaras08", "datospersonales");
-         
-         if ($miconn->connect_errno) {
-            echo "Fallo al conectar a MySQL: (" . $miconn->connect_errno . ") " . $miconn->connect_error;
-            }
-            echo $miconn->host_info . "\n";
-            
-            /* Consultas de selección que devuelven un conjunto de resultados */
-            if ($resultado = $miconn->query("SELECT * FROM persona"))
-                echo "La selección devolvió N°". $resultado->num_rows."filas";
-                /* liberar el conjunto de resultados */
-                $resultado->close();
-        ?>
+        <a href="formulario.php">Formulario</a>
 </html>

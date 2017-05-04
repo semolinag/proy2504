205917d5d9eff773e53efc9ec9685a880315274c
cetecom <cetecom@LC08L1203>
2017-05-04 12:23:54 -0300

    HOLA

--- index.php
+++ index.php
@@ -10,5 +10,18 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <?php
+         $miconn=new mysqli("10.20.25.214", "root", "avaras08", "datosPersonales");
+         
+         echo ($miconn->host_info);
+         
+         if ($miconn->connect_errno) {
+            echo "Fallo al conectar a MySQL: (" . $miconn->connect_errno . ") " . $miconn->connect_error;
+            }
+            /* Consultas de selección que devuelven un conjunto de resultados */
+            if ($resultado = $miconn->query("SELECT * FROM persona"))
+                
+                $resultado->close();
+        ?>
         <a href="formulario.php">Formulario</a>
 </html>
